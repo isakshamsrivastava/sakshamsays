bc1286929dda5289349a76cb70d3db5575346787
Saksham Srivastava <saksham.srivastava@costrategix.com>
2025-04-23 16:32:25 +0530

    Updated UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Blog from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Blog from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Blog />
   </React.StrictMode>
-)
+);
